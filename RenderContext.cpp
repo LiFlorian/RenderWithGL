1bda79cf6d9401ea40ba79e7562ba8716f0472a2
deckardli <deckardli@tencent.com>
2023-03-20 21:44:17 +0800

    Code format uniform

--- RenderContext.cpp
+++ RenderContext.cpp
@@ -7,37 +7,37 @@
 
 RenderContext::RenderContext()
 {
-    BindVertexData();
+	BindVertexData();
 }
 
 RenderContext::~RenderContext()
 {
-    glDeleteVertexArrays(1, &this->VAO);
-    glDeleteBuffers(1, &this->VBO);
-    glDeleteBuffers(1, &this->EBO);
+	glDeleteVertexArrays(1, &this->VAO);
+	glDeleteBuffers(1, &this->VBO);
+	glDeleteBuffers(1, &this->EBO);
 }
 
 void RenderContext::BindVertexData()
 {
-    // 创建VAO并绑定
-    glGenVertexArrays(1, &this->VAO);
-    glBindVertexArray(this->VAO);
+	// 创建VAO并绑定
+	glGenVertexArrays(1, &this->VAO);
+	glBindVertexArray(this->VAO);
 
-    // 创建VBO并把顶点数组复制到缓冲中供OpenGL使用
-    glGenBuffers(1, &this->VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Rectangle_2D), Rectangle_2D, GL_STATIC_DRAW);
+	// 创建VBO并把顶点数组复制到缓冲中供OpenGL使用
+	glGenBuffers(1, &this->VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Rectangle_2D), Rectangle_2D, GL_STATIC_DRAW);
 
-    // 创建EBO并复制到缓冲
-    glGenBuffers(1, &this->EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Rectangle_2D_Indices), Rectangle_2D_Indices, GL_STATIC_DRAW);
+	// 创建EBO并复制到缓冲
+	glGenBuffers(1, &this->EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Rectangle_2D_Indices), Rectangle_2D_Indices, GL_STATIC_DRAW);
 
-    // 设置顶点属性指针
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+	// 设置顶点属性指针
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    glBindVertexArray(0);
+	glBindVertexArray(0);
 }
--- Shader.cpp
+++ Shader.cpp
@@ -5,113 +5,113 @@
 
 Shader::Shader(const char* vertexPath, const char* fragmentPath)
 {
-    // 从文件路径中获取顶点/片段着色器
-    std::string vertexCode;
-    std::string fragmentCode;
+	// 从文件路径中获取顶点/片段着色器
+	std::string vertexCode;
+	std::string fragmentCode;
 
-    std::ifstream vShaderFile;
-    std::ifstream fShaderFile;
+	std::ifstream vShaderFile;
+	std::ifstream fShaderFile;
 
-    // 保证ifstream对象可以抛出异常：
-    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    try
-    {
-        // 打开文件
-        vShaderFile.open(vertexPath);
-        fShaderFile.open(fragmentPath);
+	// 保证ifstream对象可以抛出异常：
+	vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	try
+	{
+		// 打开文件
+		vShaderFile.open(vertexPath);
+		fShaderFile.open(fragmentPath);
 
-        // 读取文件的缓冲内容到数据流中
-        std::stringstream vShaderStream, fShaderStream;
-        vShaderStream << vShaderFile.rdbuf();
-        fShaderStream << fShaderFile.rdbuf();
+		// 读取文件的缓冲内容到数据流中
+		std::stringstream vShaderStream, fShaderStream;
+		vShaderStream << vShaderFile.rdbuf();
+		fShaderStream << fShaderFile.rdbuf();
 
-        // 关闭文件处理器
-        vShaderFile.close();
-        fShaderFile.close();
+		// 关闭文件处理器
+		vShaderFile.close();
+		fShaderFile.close();
 
-        // 转换数据流到string
-        vertexCode = vShaderStream.str();
-        fragmentCode = fShaderStream.str();
-    }
-    catch (std::ifstream::failure e)
-    {
-        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
-    }
+		// 转换数据流到string
+		vertexCode = vShaderStream.str();
+		fragmentCode = fShaderStream.str();
+	}
+	catch (std::ifstream::failure e)
+	{
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+	}
 
-    const char* vShaderCode = vertexCode.c_str();
-    const char* fShaderCode = fragmentCode.c_str();
+	const char* vShaderCode = vertexCode.c_str();
+	const char* fShaderCode = fragmentCode.c_str();
 
-    // 创建vertexShader
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	// 创建vertexShader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
-    // 绑定源码并编译
-    glShaderSource(vertexShader, 1, &vShaderCode, NULL);
-    glCompileShader(vertexShader);
+	// 绑定源码并编译
+	glShaderSource(vertexShader, 1, &vShaderCode, NULL);
+	glCompileShader(vertexShader);
 
-    // vertex编译异常捕获
-    int  success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
+	// vertex编译异常捕获
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
 
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fShaderCode, NULL);
-    glCompileShader(fragmentShader);
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fShaderCode, NULL);
+	glCompileShader(fragmentShader);
 
-    // fragment编译异常捕获
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
+	// fragment编译异常捕获
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
 
-    // 创建绘制Program
-    this->ID = glCreateProgram();
+	// 创建绘制Program
+	this->ID = glCreateProgram();
 
-    // 链接shader
-    glAttachShader(this->ID, vertexShader);
-    glAttachShader(this->ID, fragmentShader);
-    glLinkProgram(this->ID);
+	// 链接shader
+	glAttachShader(this->ID, vertexShader);
+	glAttachShader(this->ID, fragmentShader);
+	glLinkProgram(this->ID);
 
-    // 链接异常捕获
-    glGetProgramiv(this->ID, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(this->ID, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::LINK_FAILED\n" << infoLog << std::endl;
-    }
+	// 链接异常捕获
+	glGetProgramiv(this->ID, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(this->ID, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::LINK_FAILED\n" << infoLog << std::endl;
+	}
 
-    // 删除Shader缓存
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+	// 删除Shader缓存
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 }
 
 Shader::~Shader()
 {
-    glDeleteProgram(this->ID);
+	glDeleteProgram(this->ID);
 }
 
 void Shader::Use()
 {
-    glUseProgram(this->ID);
+	glUseProgram(this->ID);
 }
 
 void Shader::SetBool(const std::string& name, bool value) const
 {
-    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
 }
 void Shader::SetInt(const std::string& name, int value) const
 {
-    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 }
 void Shader::SetFloat(const std::string& name, float value) const
 {
-    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 }
