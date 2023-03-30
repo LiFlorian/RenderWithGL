af7cdd0e0bf14cb6e9460d95579e4a89e6b1ec1a
deckardli <deckardli@tencent.com>
2023-03-30 23:34:23 +0800

    add annotaion for PBR shader

--- shader/PBR/PBR.fs
+++ shader/PBR/PBR.fs
@@ -70,16 +70,15 @@ void main()
     vec3 N = normalize(Normal);
     vec3 V = normalize(ViewPos - WorldPos);
 
-    // calculate reflectance at normal incidence; if dia-electric (like plastic) use F0 
-    // of 0.04 and if it's a metal, use the albedo color as F0 (metallic workflow)    
+    // 菲涅尔参数, 非金属默认0.04, 金属度插值得出
     vec3 F0 = vec3(0.04); 
     F0 = mix(F0, albedo, metallic);
 
-    // reflectance equation
+    // 反射方程
     vec3 Lo = vec3(0.0);
     for(int i = 0; i < 4; ++i) 
     {
-        // calculate per-light radiance
+        // Li(p, wi), 光源i的辐射率
         vec3 L = normalize(lightPositions[i] - WorldPos);
         vec3 H = normalize(V + L);
         float distance = length(lightPositions[i] - WorldPos);
@@ -87,41 +86,33 @@ void main()
         vec3 radiance = lightColors[i] * attenuation;
 
         // Cook-Torrance BRDF
-        float NDF = DistributionGGX(N, H, roughness);   
-        float G   = GeometrySmith(N, V, L, roughness);      
-        vec3 F    = fresnelSchlick(clamp(dot(H, V), 0.0, 1.0), F0);
-           
+        float NDF = DistributionGGX(N, H, roughness); // 法线分布函数
+        float G   = GeometrySmith(N, V, L, roughness); // 几何函数
+        vec3 F    = fresnelSchlick(clamp(dot(H, V), 0.0, 1.0), F0); // 反射函数, 不同方向, 所以返回值是vec3
+        
+        // 反射量计算
         vec3 numerator    = NDF * G * F; 
-        float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.0001; // + 0.0001 to prevent divide by zero
+        float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.0001; // 0.0001防止分母为0
         vec3 specular = numerator / denominator;
         
-        // kS is equal to Fresnel
-        vec3 kS = F;
-        // for energy conservation, the diffuse and specular light can't
-        // be above 1.0 (unless the surface emits light); to preserve this
-        // relationship the diffuse component (kD) should equal 1.0 - kS.
-        vec3 kD = vec3(1.0) - kS;
-        // multiply kD by the inverse metalness such that only non-metals 
-        // have diffuse lighting, or a linear blend if partly metal (pure metals
-        // have no diffuse light).
-        kD *= 1.0 - metallic;	  
+        // 镜面反射系数KS = 菲涅尔计算结果F
+        vec3 kD = vec3(1.0) - F;
+        kD *= 1.0 - metallic; // 金属没有漫反射, 所以这里乘以(1 - metallic)
 
-        // scale light by NdotL
+        // n*wi
         float NdotL = max(dot(N, L), 0.0);        
 
-        // add to outgoing radiance Lo
-        Lo += (kD * albedo / PI + specular) * radiance * NdotL;  // note that we already multiplied the BRDF by the Fresnel (kS) so we won't multiply by kS again
+        // 对应光线的完整辐射率
+        Lo += (kD * albedo / PI + specular) * radiance * NdotL;
     }   
     
-    // ambient lighting (note that the next IBL tutorial will replace 
-    // this ambient lighting with environment lighting).
     vec3 ambient = vec3(0.03) * albedo * ao;
 
     vec3 color = ambient + Lo;
 
-    // HDR tonemapping
+    // HDR映射
     color = color / (color + vec3(1.0));
-    // gamma correct
+    // Gamma校正
     color = pow(color, vec3(1.0 / 2.2)); 
 
     FragColor = vec4(color, 1.0);
