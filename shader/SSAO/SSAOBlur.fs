cfb6712ae4d891604f1c940baffe0be578552ebd
deckardli <deckardli@tencent.com>
2023-03-30 21:07:39 +0800

    SSAO Bug fix with PBR ready scene

--- shader/SSAO/SSAOBlur.fs
+++ shader/SSAO/SSAOBlur.fs
@@ -16,7 +16,7 @@ void main() {
         for (int y = -2; y < 2; ++y) 
         {
             vec2 offset = vec2(float(x), float(y)) * texelSize;
-            result += texture(ssaoInput, TexCoords + offset).r;
+            result += texture(SSAOTex, TexCoords + offset).r;
         }
     }
 
